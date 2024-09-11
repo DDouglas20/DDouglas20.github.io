df03b55c62b1e7751699dbc127110c33e304301a
Ddouglas20 <DeQuan.Douglas2020@gmail.com>
2024-09-10 22:01:44 -0400

    Initial Commit

---
+++ DouglasPipingHeating/DouglasPipingHeatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DouglasPipingHeatingApp.swift
+//  DouglasPipingHeating
+//
+//  Created by DeQuan Douglas on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DouglasPipingHeatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
